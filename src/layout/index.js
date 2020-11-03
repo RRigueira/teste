b3218f8973169de8a2f3555cbaed5402c32022b4
Rui <ruirigueira@others.pt>
2020-11-03 15:39:34 +0000

    fix commit

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,13 +1,11 @@
 import React from "react"
 import { Global } from "@emotion/core"
 import { globalStyles } from "~theme/global"
-import CreateHead from "../components/createHead/CreateHead"
 
 const Layout = ({ location, ...props }) => {
   return (
     <>
       <Global styles={globalStyles} />
-      <CreateHead></CreateHead>
       {props.children}
     </>
   )
